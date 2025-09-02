e9bca75acd5ffa8a1173c72e05b90f832eac79ef
adexp01 <andreydexp01@gmail.com>
2025-09-02 14:51:50 +0300

    fix

--- src/app/providers/provider.jsx
+++ src/app/providers/provider.jsx
@@ -1,6 +1,7 @@
 import { PrivyProvider } from "@privy-io/react-auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { base } from "viem/chains";
 
 import { privyClientConfig } from "../configs/privyConfig";
 
@@ -8,7 +9,10 @@ export default function Provider({ children }) {
   const appId = import.meta.env.VITE_PRIVY_APP_ID;
   const queryClient = new QueryClient();
   return (
-    <PrivyProvider appId={appId} config={privyClientConfig}>
+    <PrivyProvider
+      appId={appId}
+      config={{ ...privyClientConfig, defaultChain: base }}
+    >
       <QueryClientProvider client={queryClient}>
         {children}
         <ReactQueryDevtools initialIsOpen={false} />
